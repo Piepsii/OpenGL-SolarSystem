64af7078c831f9de4de527f9d82c5f2b396bb485
Piepsii <paulbrandstetter@tuta.io>
2021-11-12 10:13:47 +0100

    Initial commit

---
+++ spinach/src/main.cpp
@@ -0,0 +1,16 @@
+// main.cpp
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+int main(int argc, char **argv)
+{
+   if (!glfwInit()) {
+      return 0;
+   }
+   
+
+   return 0;
+}
+
