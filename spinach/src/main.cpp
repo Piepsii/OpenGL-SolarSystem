cca51297615a6c57fd57073ee0668ee7da467cd7
Piepsii <paulbrandstetter@tuta.io>
2021-11-16 14:54:26 +0100

    Rotating Cubes

--- spinach/src/main.cpp
+++ spinach/src/main.cpp
@@ -13,19 +13,122 @@ const static float ASPECT = 16.0f / 9.0f;
 const static float NEAR = 1.0f;
 const static float FAR = 100.0f;
 
+float rotationSpeed = 0.1f;
+float cursorPosX = 0.0f, cursorPosY = 0.0f;
 
 struct vertex {
     float x, y, z;
     float r, g, b, a;
 };
 
-const vertex triangle[] = {
-    // position (x, y, z)    color (r, g, b, a)
-    { 0.0f,  1.0f,  0.0f,    1.0f, 0.0f, 0.0f, 1.0f },
-    { 1.0f,  -1.0f, 0.0f,    0.0f, 1.0f, 0.0f, 1.0f },
-    { -1.0f, -1.0f, 0.0f,    0.0f, 0.0f, 1.0f, 1.0f },
+struct Mesh {
+    int m_offset;
+    int m_count;
 };
 
+const vertex sixSidedCube[] = {
+    // front
+    { 1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  -1.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f,  1.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+
+    // left
+    { -1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, -1.0f,    0.0f, 1.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f },
+    { -1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f },
+    { -1.0f,  1.0f, -1.0f,    0.0f, 1.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, -1.0f,    0.0f, 1.0f, 0.0f, 1.0f },
+
+    // right
+    { 1.0f,  1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f },
+    { 1.0f, -1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f },
+    { 1.0f, -1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f },
+    { 1.0f,  1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f },
+    { 1.0f,  1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f },
+    { 1.0f, -1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f },
+
+    // back
+    { -1.0f, -1.0f, -1.0f,    1.0f, 1.0f, 0.0f, 1.0f },
+    { 1.0f,  1.0f,  -1.0f,    1.0f, 1.0f, 0.0f, 1.0f },
+    { 1.0f,  -1.0f, -1.0f,    1.0f, 1.0f, 0.0f, 1.0f },
+    { -1.0f,  1.0f, -1.0f,    1.0f, 1.0f, 0.0f, 1.0f },
+    { 1.0f,  1.0f,  -1.0f,    1.0f, 1.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, -1.0f,    1.0f, 1.0f, 0.0f, 1.0f },
+
+    // top
+    {  1.0f, 1.0f, -1.0f,    0.0f, 1.0f, 1.0f, 1.0f },
+    { -1.0f, 1.0f,  1.0f,    0.0f, 1.0f, 1.0f, 1.0f },
+    {  1.0f, 1.0f,  1.0f,    0.0f, 1.0f, 1.0f, 1.0f },
+    {  1.0f, 1.0f, -1.0f,    0.0f, 1.0f, 1.0f, 1.0f },
+    { -1.0f, 1.0f, -1.0f,    0.0f, 1.0f, 1.0f, 1.0f },
+    { -1.0f, 1.0f,  1.0f,    0.0f, 1.0f, 1.0f, 1.0f },
+
+    // bottom
+    { -1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 1.0f, 1.0f },
+    {  1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 1.0f, 1.0f },
+    {  1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 1.0f, 1.0f },
+    { -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 1.0f, 1.0f },
+    {  1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 1.0f, 1.0f },
+    { -1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 1.0f, 1.0f },
+
+
+};
+
+const vertex oneSidedCube[] = {
+    // front
+    { 1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  -1.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f,  1.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+
+    // left
+    { -1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+
+    // right
+    { 1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+
+    // back
+    { -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  1.0f,  -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { 1.0f,  1.0f,  -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+
+    // top
+    {  1.0f, 1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, 1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    {  1.0f, 1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    {  1.0f, 1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, 1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, 1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+
+    // bottom
+    { -1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    {  1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    {  1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    {  1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+    { -1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 0.0f, 1.0f },
+
+
+};
 const char* glsl_vertex = R"(
 #version 330
 
@@ -56,6 +159,8 @@ void main(){
 
 int main(int argc, char **argv)
 {
+    /*------------------------------------------------WINDOW---------------------------------------------*/
+
     glfwSetErrorCallback([](int code, const char* message) {
         printf("!!! %s (%d)\n", message, code);
     });
@@ -66,7 +171,7 @@ int main(int argc, char **argv)
 
    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-   GLFWwindow* window = glfwCreateWindow(1920, 1080, "Triangle", nullptr, nullptr);
+   GLFWwindow* window = glfwCreateWindow(1920, 1080, "Cube", nullptr, nullptr);
    if (window == nullptr) {
        return 0;
    }
@@ -82,6 +187,19 @@ int main(int argc, char **argv)
            glfwSetWindowShouldClose(window, GLFW_TRUE);
        }
     });
+
+   glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
+       rotationSpeed += (float)(yoffset / 1000.0);
+       });
+
+   glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
+       cursorPosX = ((float)xpos / 1920.0f - 0.5f) * 2.0f;
+       cursorPosY = ((float)ypos / 1080.0f - 0.5f) * 2.0f;
+   });
+   /*------------------------------------------------WINDOW---------------------------------------------*/
+
+   /*------------------------------------------------SHADER---------------------------------------------*/
+
 
    GLuint vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
    glShaderSource(vertex_shader_id, 1, &glsl_vertex, nullptr);
@@ -119,13 +237,15 @@ int main(int argc, char **argv)
        printf("!!! shader program error\n%s\n", program_error);
    }
 
+
+   /*------------------------------------------------SHADER---------------------------------------------*/
+
+   /*------------------------------------------------MESHES---------------------------------------------*/
+
    const GLint projection_location = glGetUniformLocation(shader_program_id, "u_projection");
    const GLint world_location = glGetUniformLocation(shader_program_id, "u_world");
 
-   glm::mat4 projection = glm::perspective(PI * 0.25f, ASPECT, NEAR, FAR);
-   glm::mat4 world = glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.0f, -10.0f));
-   glm::mat4 world2 = glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 1.0f, -11.0f));
-
+   // Cube 1
    GLuint vertex_array_id = 0;
    glGenVertexArrays(1, &vertex_array_id);
    glBindVertexArray(vertex_array_id);
@@ -133,24 +253,56 @@ int main(int argc, char **argv)
    GLuint vertex_buffer_id = 0;
    glGenBuffers(1, &vertex_buffer_id);
    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id);
-   glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+   glBufferData(GL_ARRAY_BUFFER, sizeof(sixSidedCube), sixSidedCube, GL_STATIC_DRAW);
 
    glEnableVertexAttribArray(0);;
    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, x));
 
    glEnableVertexAttribArray(1);
-   glVertexAttribPointer(1, 4, GL_FLOAT, GL_TRUE, sizeof(vertex), (void*)offsetof(vertex, a));
+   glVertexAttribPointer(1, 4, GL_FLOAT, GL_TRUE, sizeof(vertex), (void*)offsetof(vertex, r));
 
-   const int primitive_count = sizeof(triangle) / sizeof(triangle[0]);
+   // Cube 2
+   GLuint vertex_array_id2 = 1;
+   glGenVertexArrays(1, &vertex_array_id2);
+   glBindVertexArray(vertex_array_id2);
+
+   GLuint vertex_buffer_id2 = 1;
+   glGenBuffers(1, &vertex_buffer_id2);
+   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id2);
+   glBufferData(GL_ARRAY_BUFFER, sizeof(oneSidedCube), oneSidedCube, GL_STATIC_DRAW);
+
+   glEnableVertexAttribArray(0);;
+   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, x));
+
+   glEnableVertexAttribArray(1);
+   glVertexAttribPointer(1, 4, GL_FLOAT, GL_TRUE, sizeof(vertex), (void*)offsetof(vertex, r));
+
+   const int primitive_count = sizeof(sixSidedCube) / sizeof(sixSidedCube[0]);
+   float rotation = 0.0f;
+
+   /*------------------------------------------------MESHES---------------------------------------------*/
 
    while (!glfwWindowShouldClose(window)) {
        int width = 0, height = 0;
        glfwGetFramebufferSize(window, &width, &height);
 
+       rotation += rotationSpeed;
+
+       glm::mat4 projection = glm::perspective(PI * 0.25f, ASPECT, NEAR, FAR);
+       glm::mat4 world[2];
+       world[0] = glm::translate(glm::mat4(1.0f), glm::vec3(-2.0f + cursorPosX, 0.0f - cursorPosY, -7.0f)) *
+           glm::rotate(glm::mat4(1.0f), rotation, glm::normalize(glm::vec3(1.0f, 1.0f, 0.0f)));
+       world[1] = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f + cursorPosX, 0.0f - cursorPosY, -7.0f)) *
+           glm::rotate(glm::mat4(1.0f), rotation, glm::normalize(glm::vec3(1.0f, 1.0f, 0.0f)));
+
+
        glClearDepth(1.0f);
        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
        glViewport(0, 0, width, height);
        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+       glEnable(GL_CULL_FACE);
+       glCullFace(GL_BACK);
 
        glEnable(GL_DEPTH_TEST);
 
@@ -158,10 +310,12 @@ int main(int argc, char **argv)
        glUniformMatrix4fv(projection_location, 1, GL_FALSE, glm::value_ptr(projection));
        glBindVertexArray(vertex_array_id);
 
-       glUniformMatrix4fv(world_location, 1, GL_FALSE, glm::value_ptr(world));
+       glUniformMatrix4fv(world_location, 1, GL_FALSE, glm::value_ptr(world[0]));
        glDrawArrays(GL_TRIANGLES, 0, primitive_count);
 
-       glUniformMatrix4fv(world_location, 1, GL_FALSE, glm::value_ptr(world2));
+       glBindVertexArray(vertex_array_id2);
+
+       glUniformMatrix4fv(world_location, 1, GL_FALSE, glm::value_ptr(world[1]));
        glDrawArrays(GL_TRIANGLES, 0, primitive_count);
 
 
